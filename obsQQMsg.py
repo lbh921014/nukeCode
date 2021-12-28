5264c318fe909336f332127af0163d69afd3a675
lbh921014 <lbh921014@163.com>
2021-12-28 15:46:58 +0800

    combine

--- obsQQMsg.py
+++ obsQQMsg.py
@@ -1,7 +1,14 @@
 from flask import Flask, request
 
+import asyncio
+import time
+import json
+import requests
+import datetime
+from playwright.sync_api import sync_playwright
+
 '''注意，这里的import api是另一个py文件，下文会提及'''
-import nukeCode
+# import nukeCode
 
 app = Flask(__name__)
 
@@ -17,8 +24,8 @@ def post_data():
         # api.keyword(message, uid)  # 将 Q号和原始信息传到我们的后台
         if uid == 254923340 and message == '核弹密码':
             print('触发条件')
-            nukeCode.sendCode()
-
+            # nukeCode.sendCode()
+            sendCode()
 
     if request.get_json().get('message_type') == 'group':  # 如果是群聊信息
         gid = request.get_json().get('group_id')  # 获取群号
@@ -28,7 +35,88 @@ def post_data():
         print(message)
         if gid == 256615371 and message == '核弹密码':
             print('触发条件')
-            nukeCode.sendCodeGroup()
+            sendCodeGroup()
     return "OK"
+
+def sendCode():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        page = context.new_page()
+        page.goto("https://nukacrypt.com/")
+        time.sleep(1)
+        rows = page.query_selector_all('div[id="nuclearcodess"] table[class="contenttable"] tbody tr')
+        print(len(rows))
+        i = 0
+        title = ""
+        exp_time = ""
+        code_list_str = ""
+        code_list = []
+        for row in rows:
+            if i == 1:
+                title = row.inner_text()
+            if i == 2:
+                exp_time = row.inner_text()
+            if i == 3:
+                code_list_str = row.inner_text()
+                elements = row.query_selector_all('td')
+                for item in elements:
+                    item.inner_text()
+                    code_list.append(item.inner_text())
+            i += 1
+        print("title=%s" % title)
+        print("expTime=%s" % exp_time)
+        print("codeListStr=%s" % code_list_str)
+        print(code_list)
+        page.close()
+        context.close()
+        browser.close()
+
+        data = {"user_id": "254923340", "message": title + '\n' + exp_time + '\n' + code_list_str}
+        response = requests.post("http://127.0.0.1:5900/send_private_msg", data=data)
+        print(response)
+
+def sendCodeGroup():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        context = browser.new_context()
+        date = datetime.datetime.now().strftime("%Y-%m-%d")
+        page = context.new_page()
+        page.goto("https://nukacrypt.com/")
+        time.sleep(1)
+        rows = page.query_selector_all('div[id="nuclearcodess"] table[class="contenttable"] tbody tr')
+        print(len(rows))
+        i = 0
+        title = ""
+        exp_time = ""
+        code_list_str = ""
+        code_list = []
+        for row in rows:
+            if i == 1:
+                title = row.inner_text()
+            if i == 2:
+                exp_time = row.inner_text()
+            if i == 3:
+                code_list_str = row.inner_text()
+                elements = row.query_selector_all('td')
+                for item in elements:
+                    item.inner_text()
+                    code_list.append(item.inner_text())
+            i += 1
+        print("title=%s" % title)
+        print("expTime=%s" % exp_time)
+        print("codeListStr=%s" % code_list_str)
+        print(code_list)
+        page.close()
+        context.close()
+        browser.close()
+
+        data = {"group_id": "256615371", "message": title + '\n' + exp_time + '\n' + code_list_str}
+        response = requests.post("http://127.0.0.1:5900/send_group_msg", data=data)
+        # data = {"user_id": "254923340", "message": title + '\n' + exp_time + '\n' + code_list_str}
+        # response = requests.post("http://127.0.0.1:5900/send_private_msg", data=data)
+        # res = json.loads(response)
+        print(response)
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8001, debug=True)  # 此处的 host和 port对应上面 yml文件的设置
